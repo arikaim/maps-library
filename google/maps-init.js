d1ae2d0c68e3490d19492418aea3d1af3530d48a
Arikaim <info@arikaim.com>
2021-11-23 20:25:46 +0200

    =

--- google/maps-init.js
+++ google/maps-init.js
@@ -1,17 +1,1 @@
 'use strict';
-
-function getCurrentPosition(onSuccess, onError) {
-    if (!navigator.geolocation) {
-        return false;
-    }      
-    
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var position = { 
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        };
-        callFunction(onSuccess,position);
-    },function() {
-        callFunction(onError);
-    });   
-}
